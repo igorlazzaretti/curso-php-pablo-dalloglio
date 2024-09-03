d4148a1b9a7bb7c151b5b48015a7480ee5ae729a
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-09-03 00:04:16 -0300

    add: First Module

---
+++ mod-1.Introducao/1.3-estrutura.php
@@ -0,0 +1,17 @@
+Use a abertura para fins de compatibilidade,
+caso aja algo antes não será interpretado.
+
+<?php
+
+
+/*
+Comentários de 
+várias 
+linhas.
+*/
+
+// Para Imprimir
+
+
+//Pode-se omitir a tag de fechamento, porém apenas neste caso
+?>
